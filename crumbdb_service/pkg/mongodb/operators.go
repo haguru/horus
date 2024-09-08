e2ab4e9c8772b0397dab8db59d52644b7b78bb60
haguru <marc.p.fuller@gmail.com>
2024-09-08 01:11:59 -0700

    fix: add description to spatial operators
    Signed-off-by: haguru <marc.p.fuller@gmail.com>

--- crumbdb_service/pkg/mongodb/operators.go
+++ crumbdb_service/pkg/mongodb/operators.go
@@ -17,8 +17,38 @@ type SpatialQueryCommand struct {
 	Location interface{} `bson:"location"`
 }
 
+// GeoIntersectsOp selects documents whose geospatial data intersects with a specified GeoJSON object; 
+// i.e. where the intersection of the data and the specified object is non-empty.
 type GeoIntersectsOp struct {
 	GeoIntersects interface{} `bson:"$geoIntersects"`
+}
+
+// GeoWithinOP selects documents with geospatial data that exists entirely within a specified shape.
+// The specified shape can be either a GeoJSON Polygon (either single-ringed or multi-ringed), 
+// a GeoJSON MultiPolygon, or a shape defined by legacy coordinate pairs. 
+// The $geoWithin operator uses the $geometry operator to specify the GeoJSON object.
+type GeoWithinOP struct {
+	GeoWithin interface{} `bson:"$geoWithin"`
+}
+
+// NearOp specifies a point for which a geospatial query returns the documents from nearest to farthest. 
+// The $near operator can specify either a GeoJSON point or legacy coordinate point.
+type NearOp struct {
+	Near interface{} `bson:"$near"`
+}
+
+// NearSphereOP specifies a point for which a geospatial query returns the documents from nearest to farthest. 
+// MongoDB calculates distances for $nearSphere using spherical geometry.
+type NearSphereOP struct {
+	NearSphere interface{} `bson:"$nearSphere"`
+}
+
+type MaxDistanceOp struct {
+	MaxDistance int `bson:"$maxDistance"`
+}
+
+type MinDistanceOp struct {
+	MinDistance int `bson:"$minDistance"`
 }
 
 type Point struct {
@@ -30,26 +60,6 @@ type GeometryOP struct {
 	Geometry    Point `bson:"$geometry"`
 	MaxDistance int   `bson:"$maxDistance,omitempty"`
 	MinDistance int   `bson:"$minDistance,omitempty"`
-}
-
-type GeoWithinOP struct {
-	GeoWithin interface{} `bson:"$geoWithin"`
-}
-
-type NearOp struct {
-	Near interface{} `bson:"$near"`
-}
-
-type NearSphereOP struct {
-	NearSphere interface{} `bson:"$nearSphere"`
-}
-
-type MaxDistanceOp struct {
-	MaxDistance int `bson:"$maxDistance"`
-}
-
-type MinDistanceOp struct {
-	MinDistance int `bson:"$minDistance"`
 }
 
 func NewSpatialQueryCommand(opType string, pointType string, coordinates []float64, maxDistance int, minDistance int) (interface{}, error) {
