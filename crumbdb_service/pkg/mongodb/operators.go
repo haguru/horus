b0e8593f6f778fe543c6675e33d1a3d748b70f6d
haguru <marc.p.fuller@gmail.com>
2024-09-08 01:19:31 -0700

    fix: add description of update operators in useracct service
    Signed-off-by: haguru <marc.p.fuller@gmail.com>

--- crumbdb_service/pkg/mongodb/operators.go
+++ crumbdb_service/pkg/mongodb/operators.go
@@ -17,27 +17,27 @@ type SpatialQueryCommand struct {
 	Location interface{} `bson:"location"`
 }
 
-// GeoIntersectsOp selects documents whose geospatial data intersects with a specified GeoJSON object; 
+// GeoIntersectsOp selects documents whose geospatial data intersects with a specified GeoJSON object;
 // i.e. where the intersection of the data and the specified object is non-empty.
 type GeoIntersectsOp struct {
 	GeoIntersects interface{} `bson:"$geoIntersects"`
 }
 
 // GeoWithinOP selects documents with geospatial data that exists entirely within a specified shape.
-// The specified shape can be either a GeoJSON Polygon (either single-ringed or multi-ringed), 
-// a GeoJSON MultiPolygon, or a shape defined by legacy coordinate pairs. 
+// The specified shape can be either a GeoJSON Polygon (either single-ringed or multi-ringed),
+// a GeoJSON MultiPolygon, or a shape defined by legacy coordinate pairs.
 // The $geoWithin operator uses the $geometry operator to specify the GeoJSON object.
 type GeoWithinOP struct {
 	GeoWithin interface{} `bson:"$geoWithin"`
 }
 
-// NearOp specifies a point for which a geospatial query returns the documents from nearest to farthest. 
+// NearOp specifies a point for which a geospatial query returns the documents from nearest to farthest.
 // The $near operator can specify either a GeoJSON point or legacy coordinate point.
 type NearOp struct {
 	Near interface{} `bson:"$near"`
 }
 
-// NearSphereOP specifies a point for which a geospatial query returns the documents from nearest to farthest. 
+// NearSphereOP specifies a point for which a geospatial query returns the documents from nearest to farthest.
 // MongoDB calculates distances for $nearSphere using spherical geometry.
 type NearSphereOP struct {
 	NearSphere interface{} `bson:"$nearSphere"`
@@ -62,6 +62,7 @@ type GeometryOP struct {
 	MinDistance int   `bson:"$minDistance,omitempty"`
 }
 
+// NewSpatialQueryCommand returns an interface containing the spatial query operators and error if opType/pointType is unsupported
 func NewSpatialQueryCommand(opType string, pointType string, coordinates []float64, maxDistance int, minDistance int) (interface{}, error) {
 	cmd := SpatialQueryCommand{}
 	geometryOp := GeometryOP{}
