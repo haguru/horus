9296e9ad0f62869fcf6cfc10e59fbffd0ee5c019
haguru <marc.p.fuller@gmail.com>
2024-09-14 23:41:26 -0700

    Prometheus (#1)
    *feat: add prometheus grpc metrics
    
    Signed-off-by: haguru <marc.p.fuller@gmail.com>

--- follower_service/pkg/app/app.go
+++ follower_service/pkg/app/app.go
@@ -4,25 +4,40 @@ import (
 	"context"
 	"fmt"
 	"net"
+	"net/http"
+	// "time"
 
-	"github.com/go-playground/validator/v10"
 	"github.com/haguru/horus/followerdb/config"
 	"github.com/haguru/horus/followerdb/internal/routes"
 	pb "github.com/haguru/horus/followerdb/internal/routes/protos"
 	"github.com/haguru/horus/followerdb/pkg/interfaces"
 	"github.com/haguru/horus/followerdb/pkg/mongodb"
+	appMetrics "github.com/haguru/horus/followerdb/pkg/prometheus"
 
 	"github.com/edgexfoundry/go-mod-core-contracts/clients/logger"
+	"github.com/go-playground/validator/v10"
+	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/logging"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
 	"google.golang.org/grpc"
+	// "google.golang.org/grpc/health"
+	// "google.golang.org/grpc/health/grpc_health_v1"
+	grpcprom "github.com/grpc-ecosystem/go-grpc-middleware/providers/prometheus"
+	// healthpb "google.golang.org/grpc/health/grpc_health_v1"
+)
+
+const (
+	METRICS_ENDPOINT = "/metrics"
 )
 
 type App struct {
-	LoggingClient  logger.LoggingClient
 	AppCtx         context.Context
-	ServiceConfig  *config.ServiceConfig
-	Route          *routes.Route
-	GrpcServer     *grpc.Server
 	DbServerClient interfaces.DbClient
+	GrpcServer     *grpc.Server
+	LoggingClient  logger.LoggingClient
+	Route          *routes.Route
+	ServiceConfig  *config.ServiceConfig
+	metrics        *appMetrics.Metrics
 }
 
 func NewApp() (*App, error) {
@@ -50,13 +65,18 @@ func NewApp() (*App, error) {
 		return nil, err
 	}
 
-	route := routes.NewRoute(lc, &serviceConfig.Database, db, validate)
+	metrics := appMetrics.NewMetrics(serviceConfig)
+
+	// initiate routes
+	route := routes.NewRoute(lc, &serviceConfig.Database, db, validate, metrics)
+
 	return &App{
 		LoggingClient:  lc,
 		AppCtx:         context.Background(),
 		ServiceConfig:  serviceConfig,
 		DbServerClient: db,
 		Route:          route,
+		metrics:        metrics,
 	}, nil
 }
 
@@ -65,9 +85,67 @@ func (app *App) RunServer() error {
 	if err != nil {
 		return fmt.Errorf("failed to listen: %v", err)
 	}
-	app.GrpcServer = grpc.NewServer()
+
+	// Create a gRPC Server with gRPC interceptor.
+	app.GrpcServer = grpc.NewServer(
+		grpc.StatsHandler(otelgrpc.NewServerHandler()),
+		grpc.ChainUnaryInterceptor(
+			grpc.UnaryServerInterceptor(app.metrics.GrpcMetrics.UnaryServerInterceptor(grpcprom.WithExemplarFromContext(appMetrics.ExemplarFromContext))),
+			logging.UnaryServerInterceptor(appMetrics.InterceptorLogger(app.LoggingClient), logging.WithFieldsFromContext(appMetrics.LogTraceID)),
+		),
+		grpc.ChainStreamInterceptor(
+			grpc.StreamServerInterceptor(app.metrics.GrpcMetrics.StreamServerInterceptor(grpcprom.WithExemplarFromContext(appMetrics.ExemplarFromContext))),
+			logging.StreamServerInterceptor(appMetrics.InterceptorLogger(app.LoggingClient), logging.WithFieldsFromContext(appMetrics.LogTraceID)),
+		),
+	)
+
 	pb.RegisterFollowerDBServer(app.GrpcServer, app.Route)
-	app.LoggingClient.Debugf("server listening at %v", lis.Addr())
+	app.metrics.GrpcMetrics.InitializeMetrics(app.GrpcServer)
+
+	// // health service
+	// healthService := health.NewServer()
+	// // Register the health service with the gRPC server
+	// grpc_health_v1.RegisterHealthServer(app.GrpcServer, healthService)
+	// // Set the service health status
+	// healthService.SetServingStatus(app.ServiceConfig.Name, grpc_health_v1.HealthCheckResponse_SERVING)
+
+	// ticker := time.NewTicker(5 * time.Second)
+
+	// go func(app *App) {
+	// 	for {
+	// 		select {
+	// 		// case <-done:
+	// 		//     return
+	// 		case <-ticker.C:
+	// 			err := app.DbServerClient.Ping()
+	// 			if err != nil {
+	// 				// app.LoggingClient.Debug("updating health status: NOT_SERVING",)
+	// 				healthService.SetServingStatus(app.ServiceConfig.Name, healthpb.HealthCheckResponse_NOT_SERVING)
+	// 			}else{
+	// 				// app.LoggingClient.Debug("updating health status: SERVING")
+	// 				healthService.SetServingStatus(app.ServiceConfig.Name,healthpb.HealthCheckResponse_SERVING)
+	// 			}
+
+				
+	// 		}
+	// 	}
+	// }(app)
+
+	go func() {
+		metricsServer := &http.Server{Addr: fmt.Sprintf(":%d", app.ServiceConfig.Metrics.Port)}
+		muxHandler := http.NewServeMux()
+		muxHandler.Handle(METRICS_ENDPOINT, promhttp.HandlerFor(app.metrics.Registry, promhttp.HandlerOpts{
+			EnableOpenMetrics: true,
+		}))
+
+		metricsServer.Handler = muxHandler
+		app.LoggingClient.Debugf("server(prometheus) listening at %v", app.ServiceConfig.Metrics.Port)
+		if err := metricsServer.ListenAndServe(); err != nil {
+			app.LoggingClient.Error("failed to start prometheus client")
+		}
+	}()
+
+	app.LoggingClient.Debugf("server(GRPC) listening at %v", lis.Addr())
 	err = app.GrpcServer.Serve(lis)
 	if err != nil {
 		return fmt.Errorf("failed to serve: %v", err)
---
+++ follower_service/pkg/prometheus/metrics.go
@@ -0,0 +1,94 @@
+package prometheus
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/edgexfoundry/go-mod-core-contracts/clients/logger"
+	"github.com/haguru/horus/followerdb/config"
+	"go.opentelemetry.io/otel/trace"
+
+	grpc_prometheus "github.com/grpc-ecosystem/go-grpc-middleware/providers/prometheus"
+	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors"
+	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/auth"
+	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/logging"
+	"github.com/prometheus/client_golang/prometheus"
+	"google.golang.org/grpc/codes"
+	healthpb "google.golang.org/grpc/health/grpc_health_v1"
+	"google.golang.org/grpc/status"
+)
+
+var (
+	BUCKETS = []float64{0.001, 0.01, 0.1, 0.3, 0.6, 1, 3, 6, 9, 20, 30, 60, 90, 120}
+)
+
+type Metrics struct {
+	Registry *prometheus.Registry
+	GrpcMetrics  *grpc_prometheus.ServerMetrics
+	MetricServer *http.Server
+}
+
+func NewMetrics(config *config.ServiceConfig) *Metrics {
+	serverMetrics := grpc_prometheus.NewServerMetrics(
+		grpc_prometheus.WithServerHandlingTimeHistogram(
+			grpc_prometheus.WithHistogramBuckets(BUCKETS),
+		),
+	)
+	
+	metrics := &Metrics{
+		Registry: prometheus.NewRegistry(),
+		GrpcMetrics: serverMetrics,
+	}
+
+	metrics.Registry.MustRegister(metrics.GrpcMetrics)
+
+	return metrics
+}
+
+func Auth(ctx context.Context) (context.Context, error) {
+	token, err := auth.AuthFromMD(ctx, "bearer")
+	if err != nil {
+		return nil, err
+	}
+	// TODO: This is example only, perform proper Oauth/OIDC verification!
+	if token != "yolo" {
+		return nil, status.Error(codes.Unauthenticated, "invalid auth token")
+	}
+	// NOTE: You can also pass the token in the context for further interceptors or gRPC service code.
+	return ctx, nil
+}
+
+func Health(ctx context.Context, callMeta interceptors.CallMeta) bool {
+	return healthpb.Health_ServiceDesc.ServiceName != callMeta.Service
+}
+
+func InterceptorLogger(lc logger.LoggingClient) logging.Logger {
+	return logging.LoggerFunc(func(_ context.Context, lvl logging.Level, msg string, fields ...any) {
+		switch lvl {
+		case logging.LevelDebug:
+			lc.Debug(msg)
+		case logging.LevelInfo:
+			lc.Info(msg)
+		case logging.LevelWarn:
+			lc.Warn(msg)
+		case logging.LevelError:
+			lc.Error(msg)
+		default:
+			lc.Debugf("unknown level %v", lvl)
+		}
+	})
+}
+
+func LogTraceID(ctx context.Context) logging.Fields {
+	if span := trace.SpanContextFromContext(ctx); span.IsSampled() {
+		return logging.Fields{"traceID", span.TraceID().String()}
+	}
+	return nil
+}
+
+func ExemplarFromContext(ctx context.Context) prometheus.Labels {
+	if span := trace.SpanContextFromContext(ctx); span.IsSampled() {
+		return prometheus.Labels{"traceID": span.TraceID().String()}
+	}
+	return nil
+}
